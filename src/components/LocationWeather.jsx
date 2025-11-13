7c79cf47e4f8edbb22addd33d0bac35876210edf
Personal Laptop <mandymei8686@gmail.com>
2025-11-13 00:15:15 -0800

    working color changing functionality; style to be fixed / light icons

--- src/components/LocationWeather.jsx
+++ src/components/LocationWeather.jsx
@@ -1,40 +1,33 @@
-import React from 'react';
 import { useLocation, useWeather } from '../hook.js';
+import React, { useEffect } from 'react';
 
 export function LocationWeather({ lat: propLat, lon: propLon }) {
   const { location, error: locationError } = useLocation();
-
-  // location?.latitude might be undefined
-  console.log('Location info:', location);
-
   const latitude = propLat ?? location?.latitude;
   const longitude = propLon ?? location?.longitude;
-
   const { allWeather, error: weatherError } = useWeather(latitude, longitude);
-  console.log('Weather info:', allWeather);
 
-  // handle loading/error states first
+  const isDay = allWeather?.current?.is_day === 1;
+
+  useEffect(() => {
+    if (isDay) {
+      document.body.classList.remove('night');
+    } else {
+      document.body.classList.add('night');
+    }
+  }, [isDay]);
+
   if (locationError) return <div>Failed to load location.</div>;
   if (!location) return <div>Loading location...</div>;
-
   if (weatherError) return <div>Failed to load weather.</div>;
   if (!allWeather) return <div>Loading weather...</div>;
 
-  const city = allWeather.location.name
-  const region = allWeather.location.region
-
+  const city = allWeather.location.name;
+  const region = allWeather.location.region;
   const tempC = allWeather.current.temp_c + '°C';
   const tempF = allWeather.current.temp_f + '°F';
-  const temp = tempC ? tempC : tempF  // TODO: change condition for settings options
-  
+  const temp = tempC ? tempC : tempF;
   const conditionText = allWeather.current.condition.text;
-  const isDay = allWeather.current.is_day === 1;
-
-  if (isDay) {
-    document.body.style.backgroundColor = '#87CEEB'; // light blue for day; TODO: change for day theme
-  } else {
-    document.body.style.backgroundColor = '#2c3e50'; // dark blue for night; TODO: change for night theme
-  }
 
   return (
     <>
